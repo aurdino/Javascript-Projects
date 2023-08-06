d6ab02c7780e1988d1cba0fb9d4c58a58e84b964
aurdino <vr16796@gmail.com>
2023-08-06 20:15:39 +0530

    added html, css and js file

---
+++ Test/index.js
@@ -0,0 +1,25 @@
+const a = 10
+
+
+/**
+ * Nodejs is an environment for execution of the javascript code without web browser
+ * under the hood node.js is c++ program
+ * With nodejs we can, 
+ * read/write files on the computer
+ * communicate with ther computers via http
+ * create http server
+ * communicate with external databases and much more...
+ * 
+ * 
+ * 
+ */
+//material icon theme,live server 
+//prettier-code formatter 
+const b = 10
+
+function addTwoNumbers(a,b) {
+    return a+b
+}
+
+console.log("Sum of a and b is = ", addTwoNumbers(a,b))
+console.log(a, b)
